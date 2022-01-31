e70e8ae0e665ec0e25ffa685d18a4a41413cb1f6
ErnestasSku <ernestassku@gmail.com>
2022-01-31 17:45:28 +0200

    Rewrite type checker

--- src/TypeChecker.hs
+++ src/TypeChecker.hs
@@ -1,36 +1,32 @@
 module TypeChecker where
 
-import Tokenizer (Token (Keyword, Operator, Seperator), )
-import Utilites (findNextToken)
+-- import Tokenizer (Token (Keyword, Operator, Seperator), )
+import Utilites
 import Data.Maybe (isJust)
+import Parsing.AST
 
 
---Works fine. Although, it does not look too elegant.
-checkTypes :: [Token] -- Input list of tokens 
-  -> [(Int, String)]  -- Output of Line number and String. Todo: rewrite this into a new datatype which would allow for more flexibility 
-checkTypes t= reverse $ checkTypesFunc t 1 []
-    where
-    checkTypesFunc :: [Token] -> Int -> [(Int, String)] -> [(Int, String)]
-    checkTypesFunc y@(x:xs) l acc = case x of
-        Seperator "\n" -> checkTypesFunc xs (l + 1) acc
-        Keyword "var" ->
+
+typeCheck :: [AST] -> [(Int, String)]
+typeCheck (x:xs) = case x of
+  Keyword s (line, col) ->
+      if s == "var" || s == "const"
+        then
+        let
+            op = xs !! 2 -- #TODO while the assigment should always be in 2nd position (after variable name), rewrite to be more "correct" in case of unexpected input
+        in
+            if fromASTtoString op == ":" || fromASTtoString op == ":="
+                then typeCheck xs
+                else (line, "Variable is not staticly typed") : typeCheck xs
+        else
+      if s == "func"
+          then
             let
-                opList = [Operator ":=", Operator ":"]
-
-                fList = map (findNextToken y) opList
-
+                opList = filter (<! line) xs
+                boolOpList = map ((== "->") . fromASTtoString) opList
             in
-                if any isJust fList
-                then checkTypesFunc xs l acc
-                else checkTypesFunc xs l $ (l, "Variable is not staticly typed") : acc
-        Keyword "func" ->
-            let 
-                opList = [Operator "->"]
-                fList = map (findNextToken y) opList
-            in
-                if any isJust fList
-                then checkTypesFunc xs l acc
-                else checkTypesFunc xs l $ (l, "Function is not staticly typed") : acc
-        _ -> checkTypesFunc xs l acc
-    checkTypesFunc [] _ acc = acc
-
+                if or boolOpList then typeCheck xs
+                else (line, "Function is not staticly typed") : typeCheck xs
+          else typeCheck xs
+  _ -> typeCheck xs
+typeCheck [] = []
