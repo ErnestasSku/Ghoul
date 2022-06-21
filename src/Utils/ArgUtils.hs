276fbed4b83cfe1f02902c6a5e563ae03aa40c41
ErnestasSku <ernestassku@gmail.com>
2022-06-21 11:15:37 +0300

    Expand ArgSettings to accept Rule and Style Input

--- src/Utils/ArgUtils.hs
+++ src/Utils/ArgUtils.hs
@@ -14,21 +14,23 @@ import PrettyPrint.Pretty (Output(..))
 import Data.Maybe ( fromJust, isJust )
 
 class ArgDict a where
+  argKeywords :: a -> [String]
   argNames :: a -> [(String, a)]
+  argNames x = [(s, x) | s <- argKeywords x]
   matchArg :: String -> Maybe a
 
 data ArgCommands = Init | Version | Help | RunAll
   deriving (Show)
 
-data ArgSettings = Verbose | EditorInput [String]
+
+data ArgSettings = Verbose | RuleInput [String] | StyleInput [String]
   deriving (Show, Eq)
 
--- FutureNote: Could be done more cleaner
 instance ArgDict ArgCommands where
-  argNames Init = [(x, Init) | x <- ["Init", "init"]]
-  argNames Version = [(x, Version) | x <- ["Version", "version"]]
-  argNames Help = [(x, Help) | x <- ["Help", "help"]]
-  argNames RunAll = [(x, RunAll) | x <- ["RunAll", "runAll", "runall"]]
+  argKeywords Init = ["Init", "init"]
+  argKeywords Version = ["Version", "version"]
+  argKeywords Help = ["Help", "help"]
+  argKeywords RunAll = ["RunAll", "runAll", "runall"]
 
   matchArg s =
     let
@@ -40,15 +42,17 @@ instance ArgDict ArgCommands where
       init' <|> ver' <|> help' <|> run'
 
 instance ArgDict ArgSettings where
-  argNames Verbose = [(x, Verbose) | x <- ["-v", "-verbose", "-Verbose"]]
-  argNames (EditorInput _) = [(x, EditorInput []) | x <- ["-editorArgs"]]
+  argKeywords Verbose = ["-v", "-verbose", "-Verbose"]
+  argKeywords (RuleInput _) = ["-rules"]
+  argKeywords (StyleInput _) = ["-style"]
 
   matchArg s =
     let
       verb' = lookup s $ argNames Verbose
-      edit' = lookup s $ argNames (EditorInput [])
+      rule' = lookup s $ argNames (RuleInput [])
+      styl' = lookup s $ argNames (StyleInput [])
     in
-      verb' <|> edit'
+      verb' <|> rule' <|> styl'
 
 data Arguments = Arguments
   {
@@ -90,9 +94,31 @@ buildArguments s = buildArgumentsHelper s emptyArgumentList
     buildArgumentsHelper :: [String] -> Arguments -> Arguments
     buildArgumentsHelper (x:xs) args = case getArgType x of
       Comm ac -> buildArgumentsHelper xs args{argCommands = ac : argCommands args}
-      Sett (EditorInput []) -> args{argSettings = EditorInput xs : argSettings args}
+      Sett (RuleInput []) -> buildArgumentsHelper (dropInput xs) args{argSettings = RuleInput (collectInput xs) : argSettings args}
+      Sett (StyleInput []) -> buildArgumentsHelper (dropInput xs) args{argSettings = StyleInput (collectInput xs) : argSettings args}
       Sett as -> buildArgumentsHelper xs args{argSettings = as : argSettings args}
       Outp out -> buildArgumentsHelper xs args{argOutputMode = out}
     buildArgumentsHelper [] args = args
 
 
+    {-
+      a function - maps a single string to a list of strings
+      and compares them. Returns a result of bool
+
+      Example: 
+      - allSettingKeywords : list of compared strings. ex: ["a", "b", "c"]
+      - s : string which is being compared. ex: "b"
+      - @result : [False, True, False]
+    -}
+    a :: String -> [Bool]
+    a s = map ((\x -> x s) . (\x -> (==x))) allSettingKeywords
+    allSettingKeywords = argKeywords (RuleInput []) ++ argKeywords (StyleInput [])
+
+    b :: [Bool] ->  Bool
+    b = not . or
+
+    collectInput :: [String] -> [String]
+    collectInput = takeWhile (b . a)
+
+    dropInput :: [String] -> [String]
+    dropInput = dropWhile (b . a)
